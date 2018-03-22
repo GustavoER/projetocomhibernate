c172cdc518eda5b27258eb334b843a05ed29bc7e
GustavoAlvesSilva <gustavo.silva74@etec.sp.gov.br>
2018-03-22 08:29:15 -0300

    subindo

---
+++ src/main/java/Testes/TestesDelete.java
@@ -0,0 +1,20 @@
+package Testes;
+
+import modelo.Pais;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class TestesDelete {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("IFSULModelPU");
+        EntityManager em = emf.createEntityManager();
+        Pais p = em.find(Pais.class, 3);
+        em.getTransaction().begin();
+        em.remove(p);
+        em.getTransaction().commit();
+        em.close();
+        emf.close();
+    }
+}
